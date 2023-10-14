b85e6633fc12724fcefbe39662ee4af2d9c453aa
Hannah Kimura <hkimura@mit.edu>
2023-10-14 12:41:41 -0400

    test

--- server/responses.ts
+++ server/responses.ts
@@ -1,6 +1,6 @@
 import { User } from "./app";
 
-import { AlreadyFriendsError, FriendNotFoundError, FriendRequestAlreadyExistsError, FriendRequestDoc, FriendRequestNotFoundError } from "./concepts/friend";
+import { AlreadyFriendsError, FriendNotFoundError, FriendRequestAlreadyExistsError, FriendRequestDoc, FriendRequestNotFoundError } from "./concepts/connect";
 import { PostAuthorNotMatchError, PostDoc } from "./concepts/post";
 import { Router } from "./framework/router";
 
